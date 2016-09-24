ecdd48d34b2bdee8ec44107f9d1863c5692b7474
a <a@example.com>
2016-09-24 22:15:13 +0200

    Moved ipforward config

--- firewall-rules.sh
+++ firewall-rules.sh
@@ -5,4 +5,4 @@ set -e
 iptables -t nat -A POSTROUTING -s 10.99.99.0/24 ! -d 10.99.99.0/24 -j MASQUERADE
 iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 
-echo "Successfully set up IP forwarding and iptables rules"
+echo "Successfully set up iptables rules"
